480be1a27214f59111cb194764b355511bb3e287
ouharri <ouharrioutman@gmail.com>
2023-09-21 16:43:21 +0100

    setup

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -9,6 +9,7 @@ module com.biblio {
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
 //            requires eu.hansolo.tilesfx;
+
     requires java.sql;
     requires lombok;
     requires jBCrypt;
